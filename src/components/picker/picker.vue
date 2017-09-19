72e0c4abb291d7f6afaee2099b9fac78b802ba18
曹世通 <caoshitong014@pingan.com.cn>
2017-09-19 14:31:03 +0800

    dev commit

--- src/components/picker/picker.vue
+++ src/components/picker/picker.vue
@@ -1,3 +1,16 @@
+<template>
+    <a :class="className" @blur="handleBlur">
+        <div class="vp-picker-rel" ref="pickerRel" @click="pickerClick">
+            <slot>ref</slot>
+        </div>
+        <overlay :visible="false" class="vp-picker-overlay" position="center" ref="pickerOverlay" @click.native.stop="clickPickerContent();" @open="overlayOpen()">
+            <slot name="picker-content">
+                content
+            </slot>
+        </overlay>
+    </a>
+</template>
+
 <script>
     import Overlay from '../overlay';
    
@@ -152,19 +165,6 @@
         }
     }
 </script>
-
-<template>
-    <a :class="className" @blur="handleBlur">
-        <div class="vp-picker-rel" ref="pickerRel" @click="pickerClick">
-            <slot>ref</slot>
-        </div>
-        <overlay :visible="false" class="vp-picker-overlay" position="center" ref="pickerOverlay" @click.native.stop="clickPickerContent();" @open="overlayOpen()">
-            <slot name="picker-content">
-                content
-            </slot>
-        </overlay>
-    </a>
-</template>
 
 
 <style>
