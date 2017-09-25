c15a7e155bcf04b978ff39af8fc8da767fbcf23b
huangguangjie <huangguangjie441@pingan.com.cn>
2017-09-25 16:25:17 +0800

    add multiple select component

--- examples/src/components/select.vue
+++ examples/src/components/select.vue
@@ -1,27 +1,40 @@
 <template>
 <div class="content">
     <div>
-        <p>Normal</p>
+        <p>Single Select</p>
         <vp-select v-model="val" :options="list" @select="function(o){ option = o }"></vp-select>
         <span>当前选项值：{{ val }}， 选择项：{{ option }}</span>
     </div>
     <div>
-        <p>Disabled Part Options</p>
+        <p>Single Disabled Part Options</p>
         <vp-select v-model="val2" :options="list2" @select="function(o){ option2 = o }"></vp-select>
         <span>当前选项值：{{ val2 }}， 选择项：{{ option2 }}</span>
     </div>
     <div>
-        <p style="color: #ccc">Disabled</p>
-        <vp-select :disabled="true"></vp-select>
+        <p style="color: #ccc">Single Disabled</p>
+        <vp-select v-model="val3" :disabled="true" :options="list3" @select="function(o){ option3 = o }"></vp-select>
+        <span>当前选项值：{{ val3 }}， 选择项：{{ option3 }}</span>
     </div>
     <div>
-        <p>Multiple</p>
-        <vp-select></vp-select>
+        <p>Multiple Select</p>
+        <vp-select width="350" type="multiple" v-model="val4" :options="list4" @select="function(o){ option4 = o }"></vp-select>
+        <span>当前选项值：{{ val4 }}， 选择项：{{ option4 }}</span>
+    </div>
+    <div>
+        <p>Multiple Disabled Part Options</p>
+        <vp-select width="350" type="multiple" v-model="val5" :options="list5" @select="function(o){ option5 = o }"></vp-select>
+        <span>当前选项值：{{ val5 }}， 选择项：{{ option5 }}</span>
+    </div>
+    <div style="color: #ccc">
+        <p>Multiple Disabled</p>
+        <vp-select width="350" type="multiple" v-model="val6" :options="list6" :disabled="true" @select="function(o){ option6 = o }"></vp-select>
+        <span>当前选项值：{{ val6 }}， 选择项：{{ option6 }}</span>
     </div>
 </div>
 </template>
 <script>
 import { Select } from 'vpui';
+const arr = new Array(5).fill(true).map((item, i) => ({ value: i + 1, text: `option${ i + 1 }` }));
 export default {
     name: 'select-page',
     components: {
@@ -31,10 +44,27 @@ export default {
         return {
             val: 2,
             option: {},
-            list: new Array(5).fill(true).map((item, i) => ({ value: i + 1, text: `option${ i + 1 }` })),
+            list: [...arr],
+
             val2: 1,
             option2: {},
-            list2: new Array(3).fill(true).map((item, i) => ({ value: i + 1, text: `option${ i + 1 }` })).concat([{ value: 4, text: 'option4', disabled: true }, { value: 5, text: 'option5', disabled: true }])
+            list2: [...arr].concat([{ value: 6, text: 'option6', disabled: true }, { value: 7, text: 'option7', disabled: true }]),
+
+            val3: 1,
+            option3: {},
+            list3: [...arr],
+
+            val4: [2,3,5],
+            option4: {},
+            list4: [...arr],
+
+            val5: [1,5],
+            option5: {},
+            list5: [...arr].concat([{ value: 6, text: 'option6', disabled: true }, { value: 7, text: 'option7', disabled: true }]),
+
+            val6: [1,3],
+            option6: {},
+            list6: [...arr],
         }
     }
 }
--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -1,12 +1,21 @@
 <template>
 <div class="select"
     :class="{ hover: hoverSelect, focus: selectMode, disabled: disabled }"
+    :style="{ width: +this.width + 'px' }"
     @mouseenter.stop="!disabled && (hoverSelect = true)"
     @mouseleave.stop="hoverSelect = false"
     v-clickoutside="outside"
     >
     <div class="select-input" @click="toggle">
-        <div>{{ text || placeholder }}</div>
+        <div>
+            <template v-if="type === 'single'">{{ text || placeholder }}</template>
+            <template v-if="type === 'multiple'">
+                <template v-if="mulOpts.length">
+                    <span class="mul-text" v-for="item in mulOpts">{{ item.text }} <em @click.stop="removeOption(item)">&times;</em></span>
+                </template>
+                <template v-else>{{ placeholder }}</template>
+            </template>
+        </div>
     </div>
     <transition name="dropDown">
     <ul class="select-list" v-show="selectMode" @mouseleave="hoverKey = undefined">
@@ -14,14 +23,19 @@
             @mouseenter="!option.disabled && (hoverKey = option.value)"
             @click="select(option)"
             :class="{ hover: hoverKey === option.value, active: activeKey === option.value, disabled: option.disabled }"
-            :value="option.value"
-            :key="option.value">{{ option.text }}</li>
+            :key="option.value">
+                <template v-if="type === 'multiple'">
+                    <label><checkbox class="checkbox" v-model="val" :value="option.value" :disabled="option.disabled"></checkbox>{{ option.text }}</label>
+                </template>
+                <template v-else>{{ option.text }}</template>
+            </li>
     </ul>
     </transition>
 </div>
 </template>
 <script>
-import Clickoutside from '../../directives/clickoutside' //点击区域之外
+import Clickoutside from '../../directives/clickoutside'; //点击区域之外
+import Checkbox from '../checkbox';
 
 export default {
     name: 'select',
@@ -38,10 +52,11 @@ export default {
         },
         type: {
             type: String,
-            default: 'single' // single、 multiple、search、multiple-search
+            default: 'single' // single, multiple, search, multiple-search
         },
         value: null,
-        disabled: false
+        disabled: false,
+        width: Number | String
     },
     data() {
         return {
@@ -50,7 +65,8 @@ export default {
             activeKey: undefined,
             text: undefined,
             val: this.value,
-            selectMode: false
+            selectMode: false,
+            mulOpts: []
         }
     },
     methods: {
@@ -59,26 +75,48 @@ export default {
             this.selectMode = !this.selectMode;
         },
         select(option) {
-            if(this.disabled) return;
-            if(!option) return;
-            if(option.disabled) return;
             this.hoverSelect = false;
-            this.text = option.text;
-            this.val = this.activeKey = this.hoverKey = option.value;
-            this.selectMode = false;
-            this.$emit('input', this.val)
-            this.$emit('select', option)
+            switch(this.type) {
+                case 'single':
+                    if(!option) return;
+                    if(option.disabled) return;
+                    this.text = option.text;
+                    this.val = this.activeKey = this.hoverKey = option.value;
+                    this.$emit('input', this.val);
+                    this.selectMode = false;
+                    this.$emit('select', option);
+                    break;
+                case 'multiple':
+                    setTimeout(() => {
+                        this.mulOpts = this.options.filter(item => this.val.indexOf(item.value) > -1);
+                        this.$emit('input', this.val);
+                        this.$emit('select', this.mulOpts);
+                    },)
+                    break;
+                default: break;
+            }
+        },
+        removeOption(option) {
+            if(this.disabled) return;
+            this.mulOpts.splice(this.mulOpts.indexOf(option), 1);
+            this.val.splice(this.val.indexOf(option.value), 1);
+            this.$emit('input', this.val);
+            this.$emit('select', this.mulOpts);
         },
         outside() {
             this.selectMode = false;
         }
     },
     created() {
+        //针对下拉单选
         let curOption = this.options.find(item => item.value === this.value);
-        this.select(curOption)
+        this.select(curOption);
     },
     directives: {
         clickoutside: Clickoutside
+    },
+    components: {
+        Checkbox
     }
 }
 </script>
@@ -131,7 +169,32 @@ export default {
             transition: all .3s;
         }
         > div {
-            padding: 0 20px 0 6px;
+            margin: 0 20px 0 6px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+        .mul-text {
+            display: inline-block;
+            background-color: #f3f3f3;
+            padding: 5px;
+            padding-right: 16px;
+            line-height: 1;
+            margin-right: 5px;
+            border-radius: 2px;
+            position: relative;
+            transition: all .2s;
+            > em {
+                display: inline-block;
+                font-size: 16px;
+                position: absolute;
+                color: #999;
+                top: 0;
+                height: 100%;
+                width: 14px;
+                line-height: 22px;
+                text-align: center;
+            }
         }
     }
     &-list {
@@ -150,6 +213,10 @@ export default {
             line-height: 28px;
             padding: 0 6px;
             cursor: pointer;
+            > label {
+                display: block;
+                cursor: pointer;
+            }
             &.active {
                 color: #4475E8;
                 background-color: #f3f3f3;
@@ -162,18 +229,21 @@ export default {
             }
         }
     }
+    .checkbox {
+        margin-right: 5px;
+    }
 }
 
 .dropDown {
     &-enter-active,
     &-leave-active {
-        transition: all .2s;
+        transition: all .1s;
         transform-origin: center top;
     }
     &-enter,
     &-leave-to{
         opacity: 0;
-        transform: scaleY(.5);
+        transform: scaleY(.8);
         transform-origin: center top;
     }
 }
