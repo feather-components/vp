12e51cdbb5938b013032a8953f50d26ebd1bb697
yangsheng <495219123@qq.com>
2017-09-29 17:24:52 +0800

    进度条组件和文件上传组件新增

--- src/components/uploader/uploader.vue
+++ src/components/uploader/uploader.vue
@@ -1,24 +1,32 @@
 <template>
 <span>
     <a href="javascript:;" class="file">{{text}}
-        <input type="file" id="fileUpload" :multiple="multiple" :accept="fileType" @change ="uploadFile">
+        <input type="file" id="fileUpload" :multiple="multiple" :accept="fileType" @change="uploadFile">
     </a>
     <br>
-    <div v-for="(item,index) in files" v-if="item.status==0&&showProgress" :key="index">
-        <span v-if="showFileName">{{item.name}}<br></span>
-        <progress-bar :scale="item.scale" :error="item.error" type="small" @close="close(index)"></progress-bar>
-        <br>
+    <vp-mask ref="innerMask"></vp-mask>
+    <div class="progresslist" :class="progressType" v-if="showProgress&&showProgress2&&files.length>0">
+        <p class="progresstitle"><a href="javascript:" @click="closeProgress">&#10005</a>上传进度</p>
+        <div class="filelist">
+            <div v-for="(item,index) in files" v-if="item.status==0" :key="index">
+                <span v-if="showFileName">{{item.name}}<br></span>
+                <progress-bar :scale="item.scale" :error="item.error" :type="progressType" @close="close(index)"></progress-bar>
+                <br>
+            </div>
+        </div>
     </div>
 </span>
 </template>
 
 <script>
     import ProgressBar from '../progressbar';
+    import Mask from '../mask';
     export default{
         name: 'uploader',
         data(){
             return{
-                files:[]
+                files:[],
+                showProgress2:true
             }
         },
         props: {
@@ -47,6 +55,10 @@
             showFileName:{
                 type: Boolean,
                 default:false
+            },
+            progressType:{
+                type: String,
+                default:""
             }
         },
         methods:{
@@ -54,6 +66,11 @@
                 var self = this;
                 console.log(document.querySelector("#fileUpload").files);
                 var oFiles = document.querySelector("#fileUpload").files;
+                console.log(oFiles);
+                if(oFiles.length>0){
+                    self.$refs.innerMask.open();
+                    self.showProgress2 = true;
+                }
                 //length = length+oFiles.length;
                 for (let i = 0, file; file = oFiles[i]; i++) {
                     if(self.fileMaxSize>0&&file.size/1024>self.fileMaxSize*1024){
@@ -94,6 +111,7 @@
                                     }
                                     if(!uploading){
                                         self.files = [];
+                                        self.$refs.innerMask.close();
                                         self.$emit('complete',oFiles,data);
                                     }
                                     console.log("上传成功！");
@@ -137,11 +155,18 @@
                 }
                 if(!uploading){
                     self.files = [];
+                    self.$refs.innerMask.close();
                 }
+            },
+            closeProgress(){
+                let self = this;
+                self.showProgress2 = false;
+                self.$refs.innerMask.close();
             }
         },
         components: {
-            progressBar:ProgressBar
+            progressBar:ProgressBar,
+            vpMask:Mask
         }
     }
     function uploadProgress(event) {
@@ -152,7 +177,7 @@
 　　}
 </script>
 
-<style>
+<style scoped>
     .file {
         position: relative;
         display: inline-block;
@@ -178,5 +203,36 @@
         border-color: #2e67d9;
         color: #fff;
         text-decoration: none;
+    }
+    .progresslist{
+        margin-left: -195px;
+        padding: 10px;
+        width: 370px;
+        position: fixed;
+        z-index: 10001;
+        left: 50%;
+        top: 200px;
+        background-color: #fff;
+        box-shadow: 0px 0px  10px 5px #222;
+    }
+    .progresslist .filelist{
+        max-height: 300px;
+        overflow-y: scroll;
+    }
+    .progresstitle{
+        margin-bottom: 10px;
+        line-height: 24px;
+        font-size: 16px;
+    }
+    .progresstitle a{
+        width: 20px;
+        height: 20px;
+        line-height: 20px;
+        border-radius: 10px;
+        font-size: 14px;
+        color: #fff;
+        background-color: #ccc;
+        float: right;
+        text-align: center;
     }
 </style>
