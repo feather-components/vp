2a4a4bc85d1c0c9c52e0ab75da3759301e4695b8
曹世通 <caoshitong014@pingan.com.cn>
2017-12-13 17:27:55 +0800

    code commit

--- examples/src/components/tab.vue
+++ examples/src/components/tab.vue
@@ -1,46 +1,45 @@
 <template>
-<div class="tab-example">
-    <vp-tabs @changed="change1" class="vp-tab--surround">
-        <vp-tabpanel v-for="i in 10" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
-    </vp-tabs>
-    <br/>
-    <vp-tabs>
-        <vp-tabpanel v-for="i in 5" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
-    </vp-tabs>
-    <br/>
-    <vp-tabs :isManual = "true" ref="mandualTab">
-        <vp-tabpanel label="user" index="user">user</vp-tabpanel>
-        <vp-tabpanel label="role" index="role">role</vp-tabpanel>
-        <vp-tabpanel label="prvi" index="prvi">prvi</vp-tabpanel>
-        <vp-tabpanel label="red" index="red">red</vp-tabpanel>
-        <vp-tabpanel label="green" index="green">green</vp-tabpanel>
-    </vp-tabs>
-    <br/>
-    <btn @click="chooseTab($refs.mandualTab, 'user')">选择 tab</btn>
-    
-</div>
+    <div class="tab-example">
+        <vp-tabs @changed="change1" class="vp-tab--surround">
+            <vp-tabpanel v-for="i in 10" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
+        </vp-tabs>
+        <br/>
+        <vp-tabs>
+            <vp-tabpanel v-for="i in 5" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
+        </vp-tabs>
+        <br/>
+        <vp-tabs :isManual = "true" ref="mandualTab">
+            <vp-tabpanel label="user" index="user">user</vp-tabpanel>
+            <vp-tabpanel label="role" index="role">role</vp-tabpanel>
+            <vp-tabpanel label="prvi" index="prvi">prvi</vp-tabpanel>
+            <vp-tabpanel label="red" index="red">red</vp-tabpanel>
+            <vp-tabpanel label="green" index="green">green</vp-tabpanel>
+        </vp-tabs>
+        <br/>
+        <btn @click="chooseTab($refs.mandualTab, 'user')">选择 tab</btn>
+    </div>
 </template>
 <script>
-import { Tabs, TabPanel,Button } from 'vpui'
-export default {
-    name: 'tab-page',
-    components: {
-        'vp-tabs': Tabs,
-        'vp-tabpanel': TabPanel,
-        btn: Button
-    },
-    methods: {
-        change1(tab) {
-            console.log(tab);
+    import { Tabs, TabPanel,Button } from 'vpui'
+    export default {
+        name: 'tab-page',
+        components: {
+            'vp-tabs': Tabs,
+            'vp-tabpanel': TabPanel,
+            btn: Button
         },
-        change2(tab) {
-            console.log(tab);
-        },
-        chooseTab(tab, index){
-            tab.to(index);
+        methods: {
+            change1(tab) {
+                console.log(tab);
+            },
+            change2(tab) {
+                console.log(tab);
+            },
+            chooseTab(tab, index){
+                tab.to(index);
+            }
         }
     }
-}
 </script>
 <style>
     .tab-example{
