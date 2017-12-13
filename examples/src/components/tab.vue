e329467658cb48ef489827ab33b337e520b948ee
曹世通 <caoshitong014@pingan.com.cn>
2017-12-13 17:26:58 +0800

    code commit

--- examples/src/components/tab.vue
+++ examples/src/components/tab.vue
@@ -1,20 +1,33 @@
 <template>
-<div class="tab">
-    <vp-tabs id="1" @changed="change1">
-        <vp-tab v-for="i in 10" :name="'tab' + i" class="tab-panel" :key="i">{{ i }}</vp-tab>
+<div class="tab-example">
+    <vp-tabs @changed="change1" class="vp-tab--surround">
+        <vp-tabpanel v-for="i in 10" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
     </vp-tabs>
-    <vp-tabs style-type="surround" @changed="change2">
-        <vp-tab v-for="i in 5" :name="'tab' + i" class="tab-panel" :key="i">{{ i }}</vp-tab>
+    <br/>
+    <vp-tabs>
+        <vp-tabpanel v-for="i in 5" :label="'tab' + i" :index="i">{{ i }}</vp-tabpanel>
     </vp-tabs>
+    <br/>
+    <vp-tabs :isManual = "true" ref="mandualTab">
+        <vp-tabpanel label="user" index="user">user</vp-tabpanel>
+        <vp-tabpanel label="role" index="role">role</vp-tabpanel>
+        <vp-tabpanel label="prvi" index="prvi">prvi</vp-tabpanel>
+        <vp-tabpanel label="red" index="red">red</vp-tabpanel>
+        <vp-tabpanel label="green" index="green">green</vp-tabpanel>
+    </vp-tabs>
+    <br/>
+    <btn @click="chooseTab($refs.mandualTab, 'user')">选择 tab</btn>
+    
 </div>
 </template>
 <script>
-import { Tabs, Tab } from 'vpui'
+import { Tabs, TabPanel,Button } from 'vpui'
 export default {
     name: 'tab-page',
     components: {
         'vp-tabs': Tabs,
-        'vp-tab': Tab
+        'vp-tabpanel': TabPanel,
+        btn: Button
     },
     methods: {
         change1(tab) {
@@ -22,15 +35,16 @@ export default {
         },
         change2(tab) {
             console.log(tab);
+        },
+        chooseTab(tab, index){
+            tab.to(index);
         }
     }
 }
 </script>
-<style lang="less" scoped>
-.tab {
-    margin-top: 20px;
-    &-panel {
+<style>
+    .tab-example{
+        background-color: #ccc;
         padding: 20px;
     }
-}
 </style>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import CityPicker from './components/citypicker';
 import { Yearpanel, Monthpanel, Datepanel, Timepanel, Yearpicker, Monthpicker, Datepicker, Datetimepicker, Daterangepicker, Datetimerangepicker } from './components/datepicker';
 import Checkbox from './components/checkbox';
 import Radio from './components/radio';
-import { Tabs, Tab } from './components/tab';
+import { Tabs, TabPanel } from './components/tab';
 import Select from './components/select';
 import Dialog from './components/dialog';
 import { WaterFall, WaterFallItem} from './components/waterfall';
@@ -30,7 +30,7 @@ import ProgressBar from './components/progressbar';
 import Vue from 'vue';
 
 var Components = [
-    MiniPager,Pager, Datagrid, Checkbox, Radio, Tabs, Tab, Select, Dialog, Yearpanel, Monthpanel, Datepanel, Timepanel, Yearpicker, Monthpicker, Datepicker, Datetimepicker, Daterangepicker, Datetimerangepicker, WaterFall,Uploader,ProgressBar,Timeline
+    MiniPager,Pager, Datagrid, Checkbox, Radio, Tabs, TabPanel, Select, Dialog, Yearpanel, Monthpanel, Datepanel, Timepanel, Yearpicker, Monthpicker, Datepicker, Datetimepicker, Daterangepicker, Datetimerangepicker, WaterFall,Uploader,ProgressBar,Timeline
 ];
 
 var Modules = [
@@ -74,7 +74,7 @@ export {
     Checkbox,
     Radio,
     Tabs,
-    Tab,
+    TabPanel,
     Select,
     Dialog,
     Picker,
